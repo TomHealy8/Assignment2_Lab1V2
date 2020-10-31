c81dca1d1848bc8a05fe359c9f6ddf42aa8992bd
Tom Healy <tom.healy8@mail.dcu.ie>
2020-10-31 17:12:57 +0000

    Initial commit
    Assignment 2 Lab 1 - Android Studio v4.0

---
+++ app/src/main/java/com/example/assignment2_lab1v2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.assignment2_lab1v2;
+
+/**
+ * SDA Assignment 2, 2020 - Build a simple Interface
+ * https://developer.android.com/training/basics/firstapp/building-ui.html
+ */
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
